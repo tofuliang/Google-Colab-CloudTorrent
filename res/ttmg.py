eca3117e1f090a66955d2ba690a9acaffedfe58d
Biplob SD <43641536+biplobsd@users.noreply.github.com>
2019-10-29 23:44:54 +0600

    Update ttmg.py

--- res/ttmg.py
+++ res/ttmg.py
@@ -1,6 +1,6 @@
 from urllib.request import *
 import os
-from google.colab import *
+from sys import exit as exx, path as s_p
 from IPython.display import HTML, clear_output
 from lxml.etree import XML
 
@@ -35,6 +35,6 @@ def updateCheck(self, Version):
     if Version != currentVersion:
         print("Script Update Checker: Version "+currentVersion+" "+message+" Your version: "+Version+"")
         display(HTML('<div style="background-color: #4caf50!important;text-align: center;padding-top:-1px;padding-bottom: 9px;boder:1px"><h4 style="padding-top:5px"><a target="_blank" href="http://bit.ly/updateCscript" style="color: #fff!important;text-decoration: none;color: inherit;background-color:transparent;font-family: Segoe UI,Arial,sans-serif;font-weight: 400;font-size: 20px;">Open Latest Version</a></h4></div>'))
-        !kill -9 -1
+        exx()
     else:
         print("Script Update Checker: Your script is up to date")
