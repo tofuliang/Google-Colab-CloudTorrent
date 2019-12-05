66ca4df4fa5474d4399e2a2d96b08373003ceeef
Biplob SD <43641536+biplobsd@users.noreply.github.com>
2019-12-05 16:17:22 +0600

    Update doneCMD.sh

--- res/scripts/doneCMD.sh
+++ res/scripts/doneCMD.sh
@@ -8,4 +8,4 @@ fi
 
 #Upload to Gdrive
 mkdir -p "/content/drive/My Drive/$(dirname "${CLD_PATH}")"
-cp "${UP}" "/content/drive/My Drive/${CLD_PATH}"
+mv "${UP}" "/content/drive/My Drive/${CLD_PATH}"
