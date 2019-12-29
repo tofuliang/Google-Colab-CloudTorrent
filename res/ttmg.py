5ac05b10bffbd5f0c565b1f6e12196a10ba38775
Biplob SD <43641536+biplobsd@users.noreply.github.com>
2019-12-29 12:44:12 +0600

    Update ttmg.py

--- res/ttmg.py
+++ res/ttmg.py
@@ -67,7 +67,7 @@ class ngrok:
     return tokens(USR_Api)
 
 
-  def ngrok_config(token, Gport, configPath, region, service):
+  def ngrok_config(self, token, Gport, configPath, region, service):
     import os
 
     data = """
