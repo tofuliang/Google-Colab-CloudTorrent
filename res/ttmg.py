0b165841d3e93cd747260f8c7f9d38ca05e258af
Biplob SD <43641536+biplobsd@users.noreply.github.com>
2020-01-05 23:18:20 +0600

    Update ttmg.py

--- res/ttmg.py
+++ res/ttmg.py
@@ -245,12 +245,16 @@ def installNgrok():
     if checkAvailable("/usr/local/bin/ngrok"):
         return
     else:
-        runSh(
-            "wget -qq -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip"
-        )
-        runSh("unzip -qq -n ngrok-stable-linux-amd64.zip")
-        runSh("mv ngrok /usr/local/bin/ngrok")
-        runSh(f"rm -f {CWD}/ngrok-stable-linux-amd64.zip")
+        import os
+        from zipfile import ZipFile
+        from urllib.request import urlretrieve
+        
+        ngURL = "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip"
+        urlretrieve(ngURL, 'ngrok-amd64.zip')
+        with ZipFile('ngrok-amd64.zip', 'r') as zip_ref:
+            zip_ref.extractall('/usr/local/bin/')
+        os.chmod('/usr/local/bin/ngrok', 0o755)
+        os.unlink('ngrok-amd64.zip')
 
 def installAutoSSH():
     if checkAvailable("/usr/bin/autossh"):
