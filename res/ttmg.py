60da5181905bee6380abbbb4ebd4b728dcb8a9d2
Biplob SD <biplobsd11@gmail.com>
2020-03-08 15:48:00 +0600

    ngrok free tunnels command_line

--- res/ttmg.py
+++ res/ttmg.py
@@ -1,63 +1,70 @@
 import os
-from sys import exit as exx, path as s_p
+from sys import exit as exx
 HOME = os.path.expanduser("~")
 CWD = os.getcwd()
 
 # All found ngrok authtoken from github
 tokens = {
-    "lostcatbox": "1X7aYWPuFKYzvewLbnNoMo71kZi_2uzbB966Q4TU5cpgNPKhy", 
-    "zero-structure": "1UqHsShi6o3ketf426P5UtVdTfs_5XFD6sFRMkryka8fAbLd3", 
-    "ekkleesia": "7LE18LK8zeaDYeybp5fKP_6GNG1oHEfhTnQE7s9qpw", 
-    "SEARteam1928": "1Qe1IeySOQWSTnpQ3eFfr8j7Oi5_2zhanqnpZwHBhsfANd6yf", 
+    "lostcatbox": "1X7aYWPuFKYzvewLbnNoMo71kZi_2uzbB966Q4TU5cpgNPKhy",
+    "zero-structure": "1UqHsShi6o3ketf426P5UtVdTfs_5XFD6sFRMkryka8fAbLd3",
+    "ekkleesia": "7LE18LK8zeaDYeybp5fKP_6GNG1oHEfhTnQE7s9qpw",
+    "SEARteam1928": "1Qe1IeySOQWSTnpQ3eFfr8j7Oi5_2zhanqnpZwHBhsfANd6yf",
     "angenalZZZ": "7pWLVhS1gxiMAQdaFeYJy_31krnw9drNLLJftaNSFnm",
-    "lukikrk": "1XJNNnG8kZsPjjFmLsYNWCC0gIo_7VpBhwTcvhiuK4o2G2jbt", 
-    "bhavya7777": "1XzP70k7YVrg7MMaHQWPks0Q8Za_7y6b1mTDJDmJWcuqt5qTp", 
-    "hector605": "1Y14GB7E4acXxWYnVTiBejgnLuV_853z7mAgaTJxE9KY3HnCW", 
-    "fouille": "1XkoKNLcyiPECcQfGUjrTVzN64P_7tv2YgC4DSnazyVtpCpHm", 
-    "rikitz": "1Xc7z0uHxDoI9Ah06EQKgH61zoP_6WTPXDGvjFmcp2o7gNmqa", 
-    "VictorM369": "3c4WZaxPbjeRwRibY5opU_2N4TTRKaDubtEWMeKkFXn", 
-    "YHF7": "3fW4eXHdUN3ziCBXcahZ_3tnDdaTyLw8tKzJtKZkLp", 
-    "cyberkallan": "3CqeFZQht43cG5Z2YKfyv_6aKTrgrbo1HtyRi78hRKK", 
-    "Toxic-Omega": "1RCQwctVjSz8AIzHO6S55jm8XB8_5N6PqyZVnoN7mUVqF1yvT", 
-    "DevLuDaley": "1XTxsRKP8XyxvaJigX9XFXU2FvK_4dqzLxNRJHBz8A3aoPC85", 
-    "randyramig": "3Y8YSw6bvC9CsbYeRczmt_8akMuLYA3bAUshP1NCMnW", 
+    "lukikrk": "1XJNNnG8kZsPjjFmLsYNWCC0gIo_7VpBhwTcvhiuK4o2G2jbt",
+    "bhavya7777": "1XzP70k7YVrg7MMaHQWPks0Q8Za_7y6b1mTDJDmJWcuqt5qTp",
+    "hector605": "1Y14GB7E4acXxWYnVTiBejgnLuV_853z7mAgaTJxE9KY3HnCW",
+    "fouille": "1XkoKNLcyiPECcQfGUjrTVzN64P_7tv2YgC4DSnazyVtpCpHm",
+    "rikitz": "1Xc7z0uHxDoI9Ah06EQKgH61zoP_6WTPXDGvjFmcp2o7gNmqa",
+    "VictorM369": "3c4WZaxPbjeRwRibY5opU_2N4TTRKaDubtEWMeKkFXn",
+    "YHF7": "3fW4eXHdUN3ziCBXcahZ_3tnDdaTyLw8tKzJtKZkLp",
+    "cyberkallan": "3CqeFZQht43cG5Z2YKfyv_6aKTrgrbo1HtyRi78hRKK",
+    "Toxic-Omega": "1RCQwctVjSz8AIzHO6S55jm8XB8_5N6PqyZVnoN7mUVqF1yvT",
+    "DevLuDaley": "1XTxsRKP8XyxvaJigX9XFXU2FvK_4dqzLxNRJHBz8A3aoPC85",
+    "randyramig": "3Y8YSw6bvC9CsbYeRczmt_8akMuLYA3bAUshP1NCMnW",
     "sz-xcf": "1XSYq8gmxzNgMlYQzERmC50uBot_6qURZnj43KsYF2GWaUamm",
-    "api1":"6qGnEsrCL4GqZ7hMfqpyz_7ejAThUCjVnU9gD5pbP5u",
-    "api2":"1Q4i7F6isO7zZRrrjBKZzZhwsMu_74yJqoEs1HrJh1zYyxNo1",
-    "api3":"1SCsbuawjv9d79jlhlfNljaFTaB_5heVkcR6C7Sk8UBaQ1U1C",
-    "api5":"1Q45NXgsx6oyusN3GiNAYvkNJPS_AveYUDBcPHsvRvf21WZv",
-    "api6":"1Q6smHt4Bzz9VEXTwj3a7p5Gdx2_5mp6ivT6N6nB3YmRHUEM3",
-    "api7":"7VJwGkCTTUubiGhgz6Gv6_5fMLganRSKj9ntdefnF5o",
-    "api9":"5S28rBKgc22ZW7evyedNT_YvEm15RZSHdXgS4QwYbk",
-    "api12":"3VnrrXDQVHoNp9HvHFhqX_3X4JExwm6L9n6w4ppL1qy",
-    "api13":"1ShshNwfhQcyOqlMjnBDVE5X5jC_3WAmzomMHAgkunka4dSck",
-    "api14":"772yFAui6ynH9AYx29HHS_5Xcr88pHtPTQLwewv7Ctk",
-    "api16":"5HmAWwzDdkYp8CdzDQMDS_4BGwsK7AdMssLnSttZEeh",
-    "api17":"1T750atJi3xccndeUqJ4ewiS62o_2s6f8GUccL1qDUXTGSftN",
-    "api18":"1QUysRUo97w5mdB6sCZvTTMM0aK_3unoMs6nYd7grgCkuhbj3",
-    "api19":"3CqeFZQht43cG5Z2YKfyv_6aKTrgrbo1HtyRi78hRKK",
-    "api20":"5eMywZLisJNdybqpFLVgs_4XQDeF3YCMHu1Ybf7mVE6",
-    "api21":"4Cg1cEwCT7Ek89zT4VcdB_4GPAjMFgu6nhwY7SxQm94",
-    "api22":"1SGs4s9NrhxP9FRURszjL1nITSv_otcpfpb6aMVEL13u3dv1",
-    "api23":"1StL3sIccfR624Uc3BGV36XA0qG_6cAMMYFdKtPjtWax3AHSK",
-    "api24":"1SuK2ukM9Z4NohoJbU9224uMzXr_6h1ABdCrJU2EviZv4RN4r",
-    "api26":"7ecmt2Kux5uYsTUHrrqGU_3W9CJnaSeSyxiwkjxNhHc",
-    "api27":"3CqeFZQht43cG5Z2YKfyv_6aKTrgrbo1HtyRi78hRKK",
-    "api28":"2DXURjrUhAZZNMhqN5m1F_6HHzejcfRecP8upwJnNBd"
+    "api1": "6qGnEsrCL4GqZ7hMfqpyz_7ejAThUCjVnU9gD5pbP5u",
+    "api2": "1Q4i7F6isO7zZRrrjBKZzZhwsMu_74yJqoEs1HrJh1zYyxNo1",
+    "api3": "1SCsbuawjv9d79jlhlfNljaFTaB_5heVkcR6C7Sk8UBaQ1U1C",
+    "api5": "1Q45NXgsx6oyusN3GiNAYvkNJPS_AveYUDBcPHsvRvf21WZv",
+    "api6": "1Q6smHt4Bzz9VEXTwj3a7p5Gdx2_5mp6ivT6N6nB3YmRHUEM3",
+    "api7": "7VJwGkCTTUubiGhgz6Gv6_5fMLganRSKj9ntdefnF5o",
+    "api9": "5S28rBKgc22ZW7evyedNT_YvEm15RZSHdXgS4QwYbk",
+    "api12": "3VnrrXDQVHoNp9HvHFhqX_3X4JExwm6L9n6w4ppL1qy",
+    "api13": "1ShshNwfhQcyOqlMjnBDVE5X5jC_3WAmzomMHAgkunka4dSck",
+    "api14": "772yFAui6ynH9AYx29HHS_5Xcr88pHtPTQLwewv7Ctk",
+    "api16": "5HmAWwzDdkYp8CdzDQMDS_4BGwsK7AdMssLnSttZEeh",
+    "api17": "1T750atJi3xccndeUqJ4ewiS62o_2s6f8GUccL1qDUXTGSftN",
+    "api18": "1QUysRUo97w5mdB6sCZvTTMM0aK_3unoMs6nYd7grgCkuhbj3",
+    "api19": "3CqeFZQht43cG5Z2YKfyv_6aKTrgrbo1HtyRi78hRKK",
+    "api20": "5eMywZLisJNdybqpFLVgs_4XQDeF3YCMHu1Ybf7mVE6",
+    "api21": "4Cg1cEwCT7Ek89zT4VcdB_4GPAjMFgu6nhwY7SxQm94",
+    "api22": "1SGs4s9NrhxP9FRURszjL1nITSv_otcpfpb6aMVEL13u3dv1",
+    "api23": "1StL3sIccfR624Uc3BGV36XA0qG_6cAMMYFdKtPjtWax3AHSK",
+    "api24": "1SuK2ukM9Z4NohoJbU9224uMzXr_6h1ABdCrJU2EviZv4RN4r",
+    "api26": "7ecmt2Kux5uYsTUHrrqGU_3W9CJnaSeSyxiwkjxNhHc",
+    "api27": "3CqeFZQht43cG5Z2YKfyv_6aKTrgrbo1HtyRi78hRKK",
+    "api28": "2DXURjrUhAZZNMhqN5m1F_6HHzejcfRecP8upwJnNBd"
 }
+
 
 class ngrok:
 
-  def __init__(self, TOKEN=None, USE_FREE_TOKEN=True,  
-               service=[['Service1', 80, 'tcp'], ['Service2', 8080, 'tcp']], region='us',
+  def __init__(self, TOKEN=None, USE_FREE_TOKEN=True,
+               service=[['Service1', 80, 'tcp'], ['Service2', 8080, 'tcp']],
+               region='us',
                dBug=[f"{HOME}/.ngrok2/ngrok.yml", 4040]):
     self.region = region
     self.configPath, self.dport = dBug
     self.TOKEN = TOKEN
     self.USE_FREE_TOKEN = USE_FREE_TOKEN
     self.service = service
-  
-  
+    if USE_FREE_TOKEN:
+      self.sdict = {}
+      for i, sn in enumerate(service):
+        tempcP = f'{HOME}/.ngrok2/'+sn[0]+'.yml'
+        # Port, Protocol, config path
+        self.sdict[sn[0]] = [self.dport+i, sn[2], tempcP]
+
   def nameport(self, TOKEN, AUTO):
     if AUTO:
         try:
@@ -85,14 +92,16 @@ class ngrok:
 
   def ngrok_config(self, token, Gport, configPath, region, service):
     import os
-
     data = """
-    authtoken: {}
     region: {}
     update: false
     update_channel: stable
     web_addr: localhost:{}
-    tunnels:\n""".format(token, region, Gport)
+    tunnels:\n""".format(region, Gport)
+    if not self.USE_FREE_TOKEN:
+      auth ="""
+    authtoken: {}""".format(token)
+      data = auth+data
     tunnels = ""
     for S in service:
         Sn, Sp, SpC = S
@@ -104,7 +113,7 @@ class ngrok:
     os.makedirs(f'{HOME}/.ngrok2/', exist_ok=True)
     with open(configPath, "w+") as configFile:
         configFile.write(data)
-    return True  
+    return True
 
 
   def startWebUi(self, token, dport, nServer, region, btc, configPath,
@@ -122,11 +131,27 @@ class ngrok:
       clear_output()
       loadingAn(name="lds")
       textAn("Starting ngrok...", ty='twg')
-    self.ngrok_config(token, dport, configPath, region, service)
-    runSh(f"ngrok start --config {configPath} --all &", shell=True)
+    if self.USE_FREE_TOKEN:
+      for sn in service:
+        self.ngrok_config(
+          token,
+          self.sdict[nServer][0],
+          self.sdict[nServer][2],
+          region,
+          service)
+        if sn[0] == nServer:
+          runSh(f"ngrok {sn[2]} -config={self.sdict[nServer][2]} {sn[1]} &", shell=True)
+    else:
+      self.ngrok_config(token, dport, configPath, region, service)
+      runSh(f"ngrok start --config {configPath} --all &", shell=True)
     time.sleep(7)
     try:
-        host = urllib.request.urlopen(f"http://localhost:{dport}/api/tunnels")
+        if self.USE_FREE_TOKEN:
+          dport = self.sdict[nServer][0]
+          nServer = 'command_line'
+          host = urllib.request.urlopen(f"http://localhost:{dport}/api/tunnels")
+        else:
+          host = urllib.request.urlopen(f"http://localhost:{dport}/api/tunnels")
         host = loads(host.read())['tunnels']
         for h in host:
           if h['name'] == nServer:
@@ -139,7 +164,7 @@ class ngrok:
           textAn("ngrok Token is in used!. Trying another token...", ty='twg')
         time.sleep(2)
         return True
-    
+
     data = {"url": f"http://{host}"}
     if displayB:
       displayUrl(data, btc)
@@ -152,7 +177,13 @@ class ngrok:
     from json import loads
 
     try:
-      host = urllib.request.urlopen(f"http://localhost:{self.dport}/api/tunnels")
+      nServerbk = nServer
+      if self.USE_FREE_TOKEN:
+          dport = self.sdict[nServer][0]
+          nServer = 'command_line'
+      else:
+        dport = self.dport
+      host = urllib.request.urlopen(f"http://localhost:{dport}/api/tunnels")
       host = loads(host.read())['tunnels']
       for h in host:
         if h['name'] == nServer:
@@ -161,6 +192,7 @@ class ngrok:
           if displayB:
             displayUrl(data, btc)
           return data
+
       raise Exception('Not found tunnels')
     except urllib.error.URLError:
       for run in range(10):
@@ -170,7 +202,7 @@ class ngrok:
         dati = self.startWebUi(
             self.nameport(self.TOKEN, self.USE_FREE_TOKEN),
             self.dport,
-            nServer,
+            nServerbk,
             self.region,
             btc,
             self.configPath,
@@ -243,7 +275,7 @@ def displayUrl(data, btc='b', pNamU='Public URL: ', EcUrl=None, ExUrl=None):
           btshado = 'hsla(10, 40%, 52%, .4)'
 
     return display(HTML('''<style>@import url('https://fonts.googleapis.com/css?family=Source+Code+Pro:200,900');  :root {   --text-color: '''+bttxt+''';   --shadow-color: '''+btshado+''';   --btn-color: '''+btcolor+''';   --bg-color: #141218; }  * {   box-sizing: border-box; } button { position:relative; padding: 10px 20px;     border: none;   background: none;   cursor: pointer;      font-family: "Source Code Pro";   font-weight: 900;   font-size: 20px;     color: var(--text-color);      background-color: var(--btn-color);   box-shadow: var(--shadow-color) 2px 2px 22px;   border-radius: 4px;    z-index: 0;     overflow: hidden;    }  button:focus {   outline-color: transparent;   box-shadow: var(--btn-color) 2px 2px 22px; }  .right::after, button::after {   content: var(--content);   display: block;   position: absolute;   white-space: nowrap;   padding: 40px 40px;   pointer-events:none; }  button::after{   font-weight: 200;   top: -30px;   left: -20px; }   .right, .left {   position: absolute;   width: 100%;   height: 100%;   top: 0; } .right {   left: 66%; } .left {   right: 66%; } .right::after {   top: -30px;   left: calc(-66% - 20px);      background-color: var(--bg-color);   color:transparent;   transition: transform .4s ease-out;   transform: translate(0, -90%) rotate(0deg) }  button:hover .right::after {   transform: translate(0, -47%) rotate(0deg) }  button .right:hover::after {   transform: translate(0, -50%) rotate(-7deg) }  button .left:hover ~ .right::after {   transform: translate(0, -50%) rotate(7deg) }  /* bubbles */ button::before {   content: '';   pointer-events: none;   opacity: .6;   background:     radial-gradient(circle at 20% 35%,  transparent 0,  transparent 2px, var(--text-color) 3px, var(--text-color) 4px, transparent 4px),     radial-gradient(circle at 75% 44%, transparent 0,  transparent 2px, var(--text-color) 3px, var(--text-color) 4px, transparent 4px),     radial-gradient(circle at 46% 52%, transparent 0, transparent 4px, var(--text-color) 5px, var(--text-color) 6px, transparent 6px);    width: 100%;   height: 300%;   top: 0;   left: 0;   position: absolute;   animation: bubbles 5s linear infinite both; }  @keyframes bubbles {   from {     transform: translate();   }   to {     transform: translate(0, -66.666%);   } }    Resources</style><center><a href="'''+showUrL+'''" target="_blank"><div style="width: 570px;   height: 80px; padding-top:15px"><button style='--content: "'''+showTxT+'''";'">   <div class="left"></div>'''+showTxT+'''<div class="right"></div> </div></button></a></center>'''))
-    
+
 
 def findProcess(process, command="", isPid=False):
     from psutil import pids, Process
@@ -273,7 +305,7 @@ def installNgrok():
         import os
         from zipfile import ZipFile
         from urllib.request import urlretrieve
-        
+
         ngURL = "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip"
         urlretrieve(ngURL, 'ngrok-amd64.zip')
         with ZipFile('ngrok-amd64.zip', 'r') as zip_ref:
@@ -290,11 +322,11 @@ def installAutoSSH():
 
 
 def runSh(args, *, output=False, shell=False, cd=None):
-    import subprocess, shlex 
+    import subprocess, shlex
 
     if not shell:
         if output:
-            proc = subprocess.Popen( 
+            proc = subprocess.Popen(
                 shlex.split(args), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=cd
             )
             while True:
@@ -309,7 +341,7 @@ def runSh(args, *, output=False, shell=False, cd=None):
             return (
                 subprocess.run(
                     args,
-                    shell=True, 
+                    shell=True,
                     stdout=subprocess.PIPE,
                     stderr=subprocess.STDOUT,
                     cwd=cd,
@@ -317,7 +349,7 @@ def runSh(args, *, output=False, shell=False, cd=None):
                 .stdout.decode("utf-8")
                 .strip()
             )
-        return subprocess.run(args, shell=True, cwd=cd).returncode 
+        return subprocess.run(args, shell=True, cwd=cd).returncode
 
 def loadingAn(name="cal"):
       from IPython.display import HTML
@@ -329,7 +361,7 @@ def loadingAn(name="cal"):
 
 def textAn(TEXT, ty='d'):
       from IPython.display import HTML
-      
+
       if ty == 'd':
             return display(HTML('''<style>@import url(https://fonts.googleapis.com/css?family=Raleway:400,700,900,400italic,700italic,900italic);#wrapper {   font: 17px 'Raleway', sans-serif;animation: text-shadow 1.5s ease-in-out infinite;    margin-left: auto;    margin-right: auto;    }#container {    display: flex;    flex-direction: column;    float: left;     }@keyframes text-shadow { 0% 20% {          transform: translateY(-0.1em);        text-shadow:             0 0.1em 0 #0c2ffb,             0 0.1em 0 #2cfcfd,             0 -0.1em 0 #fb203b,             0 -0.1em 0 #fefc4b;    }    40% {          transform: translateY(0.1em);        text-shadow:             0 -0.1em 0 #0c2ffb,             0 -0.1em 0 #2cfcfd,             0 0.1em 0 #fb203b,             0 0.1em 0 #fefc4b;    }       60% {        transform: translateY(-0.1em);        text-shadow:             0 0.1em 0 #0c2ffb,             0 0.1em 0 #2cfcfd,             0 -0.1em 0 #fb203b,             0 -0.1em 0 #fefc4b;    }   }@media (prefers-reduced-motion: reduce) {    * {      animation: none !important;      transition: none !important;    }}</style><div id="wrapper"><div id="container">'''+TEXT+'''</div></div>'''))
       elif ty == 'twg':
@@ -338,7 +370,7 @@ def textAn(TEXT, ty='d'):
 
 def updateCheck(self, Version):
     class UpdateChecker(object):
-      
+
       def __init__(self):
           getMessage = self.getMessage
           getVersion = self.getVersion
@@ -372,10 +404,9 @@ def updateCheck(self, Version):
 
     if Version != currentVersion:
         from IPython.display import HTML
-        
+
         print("Script Update Checker: Version "+currentVersion+" "+message+" Your version: "+Version+"")
         display(HTML('<div style="background-color: #4caf50!important;text-align: center;padding-top:-1px;padding-bottom: 9px;boder:1px"><h4 style="padding-top:5px"><a target="_blank" href="http://bit.ly/updateCscript" style="color: #fff!important;text-decoration: none;color: inherit;background-color:transparent;font-family: Segoe UI,Arial,sans-serif;font-weight: 400;font-size: 20px;">Open Latest Version</a></h4></div>'))
         return True
     else:
         print("Script Update Checker: Your script is up to date")
-        
